f0003e37f3178a8bb370c7a9a52bc7916ad4caec
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-08-03 01:03:27 +0200

    bug setting ProductsList state

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,9 @@ router.get('/', function(req, res, next) {
 
 
 
-/* Create Product */
-router.post('/addProduct', async (req, res, next) => {   
+/* CREATE PRODUCT */
+/* méthode async await */
+router.post('/products', async (req, res, next) => {   
 
   const product = new productsModel ({
     prodRef: req.body.prodRef,
@@ -22,10 +23,42 @@ router.post('/addProduct', async (req, res, next) => {
     prodPrice: req.body.prodPrice
   });
   const productSaved = await product.save()
-  console.log('product saved venant du backend', product)
- 
+  console.log('product saved venant du backend', productSaved)
+  
   res.json({message: "The product has been saved", productSaved })
 });
+
+/* DISPLAY PRODUCT */
+/* méthode then */
+
+router.get('/products', (req, res, next) => {
+  productsModel.find()
+    .then(products => res.status(200).json(products))
+    .catch(error => res.status(400).json({ error }));
+});
+
+/* router.get('/products', async (req, res, next) => {
+  const productsList = await products.find()
+    
+    console.log("reponse back de la route get", productsList);
+}); */
+
+/* UPDATE PRODUCT */
+
+
+
+/* DELETE PRODUCT */
+
+router.delete('/products/:prodRef', async (req, res, next) => {
+  const returnDB = await productsModel.deleteOne({prodRef : req.params.prodRef})
+
+  let result = false
+  if(returnDb.deletedCount == 1){
+    result = true
+  }
+
+  res.json({result})
+})
 
 
 module.exports = router;
