8448aefc456f9b11e848b38323e4dba46dcc9bc5
Ophelia Tagbo <ophelia.tagbo@gmail.com>
2021-08-02 21:39:33 +0200

    add product is ok

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const productsModel = require('../models/products')
+const productsModel = require('../models/products');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,20 +10,10 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* Read Product */
-router.get('/read', async (req,res) =>{
-  res.render('index', { products });
-});
-
 
 
 /* Create Product */
 router.post('/addProduct', async (req, res, next) => {   
-
-  var result = false;
-  var error = "";
-  var message = "";
-
 
   const product = new productsModel ({
     prodRef: req.body.prodRef,
@@ -30,37 +21,11 @@ router.post('/addProduct', async (req, res, next) => {
     prodCity: req.body.prodCity, 
     prodPrice: req.body.prodPrice
   });
-  //console.log('product saved venant du backend', product)
   const productSaved = await product.save()
-  /* .then(() => res.status(201).json({ message: 'Objet enregistré !', productSaved}))
-  .catch(error => res.status(400).json({ error })); */
-  res.json({result, error, message: 'produit bien créé'})
+  console.log('product saved venant du backend', product)
+ 
+  res.json({message: "The product has been saved", productSaved })
 });
-
-
-
-/* router.post('/add', async (req,res) =>{
-  const prodRef = req.body.prodRef;
-  const prodName = req.body.prodName;
-  const prodCity = req.body.prodCity;
-  const prodPrice = req.body.prodPrice;
-
-  //console.log(req.body)
-
-  const newProduct = new productsModel({
-    prodRef = prodRef,
-    prodName = prodName,
-    prodCity = prodCity,
-    prodPrice = prodPrice,
-  });
-  const productSaved = await newProduct.save();
-
-  //console.log(productSaved);
-
-  res.json({ result: true })
-  //res.render('index', { products });
-}); */
-
 
 
 module.exports = router;
